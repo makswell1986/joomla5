8a3b8e53a90b7508233557593ec3d2ad6c1c5875
makswell1986 <103573411+makswell1986@users.noreply.github.com>
2024-06-25 23:50:36 +0500

    Delete admin directory

--- admin/src/Extension/StarsComponent.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace TechFry\Component\Stars\Administrator\Extension;
-use Joomla\CMS\Extension\MVCComponent;
-
-class StarsComponent extends MVCComponent
-{
-    
-}
-?>
--- admin/tmpl/planets/default.php
+++
@@ -1,2 +0,0 @@
-<?php
-$this->items = $this->get('Items');
